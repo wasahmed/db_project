b0dfb315a6d18b3e288b6cb1cd964c1fb5300714
Marnus Jan Botes <50694994+mjbotes@users.noreply.github.com>
2020-02-26 15:32:20 +0200

    Update MainController.java

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-package com.example.demo.model.*;
+import com.example.demo.model.*;
 
 @Controller // This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
